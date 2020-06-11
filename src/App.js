f549ab9c6279b81eb38ff788a5c9ba6507ea3b5d
yugeunyeong <kwong3@naver.com>
2020-06-11 23:26:13 +0900

    test

--- src/App.js
+++ src/App.js
@@ -5,10 +5,73 @@ import Board from "./components/task/Board";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
 
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import _ from 'lodash';
+import {Board} from './component/task/Board';
+
+let _columnId = 0;
+let _cardId = 0;
+
+const initialCards = Array.from({length: 9}).map(() => ({
+  id: ++_cardId,
+  title: `Card ${_cardId}`,
+}));
+
+const initialColumns = ['TODO', 'Doing', 'Done'].map((title, i) => ({
+  id: _columnId++,
+  title,
+  cardIds: initialCards.slice(i * 3, i * 3 + 3).map(card => card.id),
+}));
+
+
 class App extends Component {
+  state = {
+    cards: initialCards,
+    columns: initialColumns,
+  };
+
+  addCard = (columnId, _title) => {
+    const title = _title.trim();
+    if (!title) return;
+
+    const newCard = {id: ++_cardId, title};
+    this.setState(state => ({
+      cards: [...state.cards, newCard],
+      columns: state.columns.map(
+        column =>
+          column.id === columnId
+            ? {...column, cardIds: [...column.cardIds, newCard.id]}
+            : column
+      ),
+    }));
+  };
+
+  moveCard = (cardId, destColumnId, index) => {
+    this.setState(state => ({
+      columns: state.columns.map(column => ({
+        ...column,
+        cardIds: _.flowRight(
+          // 2) If this is the destination column, insert the cardId.
+          ids =>
+            column.id === destColumnId
+              ? [...ids.slice(0, index), cardId, ...ids.slice(index)]
+              : ids,
+          // 1) Remove the cardId for all columns
+          ids => ids.filter(id => id !== cardId)
+        )(column.cardIds),
+      })),
+    }));
+  };
+
   render() {
     return (
-      <div className="App">
+      <Board        
+      cards={this.state.cards}
+      columns={this.state.columns}
+      moveCard={this.moveCard}
+      addCard={this.addCard}
+      addColumn={this.addColumn}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Login}></Route>
@@ -17,9 +80,9 @@ class App extends Component {
             <Route path="/board/:id"><Board/></Route>
           </Switch>
         </BrowserRouter>
-      </div> 
+        </Board>
     );
   }
 }
 
-export default App;
+export default DragDropContext(HTML5Backend)(App);
--- src/components/task/Board.js
+++ src/components/task/Board.js
@@ -3,15 +3,46 @@ import { useParams } from 'react-router';
 import TaskList from "./TaskList";
 import './Board.css';
 
-function Board() {
+import {Column} from './Column';
+import {DraggableCard} from './Card';
+import {TextForm} from './TextForm';
+
+export function Board({cards, columns, moveCard, addCard, addColumn}) {
     var params = useParams();
     var task_id = params.id;
-        return (
-            <div className="Board">
-                <TaskList id={task_id} />
-            </div>
-        );
-    
-}
-
-export default Board;
+    return (
+      <div className="Board">
+          <TaskList id={task_id} />
+        {columns.map(column => (
+          <Column
+            key={column.id}
+            title={column.title}
+            addCard={addCard.bind(null, column.id)}
+          >
+            {column.cardIds
+              .map(cardId => cards.find(card => card.id === cardId))
+              .map((card, index) => (
+                <DraggableCard
+                  key={card.id}
+                  id={card.id}
+                  columnId={column.id}
+                  columnIndex={index}
+                  title={card.title}
+                  moveCard={moveCard}
+                />
+              ))}
+            {column.cardIds.length === 0 && (
+              <DraggableCard
+                isSpacer
+                moveCard={cardId => moveCard(cardId, column.id, 0)}
+              />
+            )}
+          </Column>
+        ))}
+        <div className="Column">
+          <TextForm onSubmit={addColumn} placeholder="Add Column..." />
+        </div>
+      </div>
+    );
+  }
+  
